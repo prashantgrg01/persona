6c683d3762e4ace7ee1aee985e30d96c7817b867
prashantgrg01 <zxcspgrg.05@live.com>
2017-11-27 18:36:12 +0545

    made some adjustments

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by("-post_date")
-    return render(request, "blog/post_list.html", {"page":"blog", "posts": posts})
+    return render(request, "blog/post_list.html", {"posts": posts})
 
 def post_detail(request, post_id):
     post = get_post(post_id)
     if post == None:
         return render(request, "404.html")
-    return render(request, "blog/post_detail.html", {"page":"blog", "post": post})
+    return render(request, "blog/post_detail.html", {"post": post})
 
 def get_post(post_id):
     try:
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "portfolio/home.html", {"page": "home"})
+    return render(request, "portfolio/home.html", {})
